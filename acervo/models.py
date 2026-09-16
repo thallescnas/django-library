0cc18825653fddcf0cabb76614f639b56772f863
thallescnas <ana.pauulajc76@gmail.com>
2026-09-16 20:53:41 -0300

    fix: corrigindo nome de choices trocado por status no ultimo commit
    feat: migrations configuradas

--- acervo/models.py
+++ acervo/models.py
@@ -5,11 +5,11 @@ from django.db import models
 
 class Tipo(models.Model):
     status = (
-        ("digital", "Livro digital")
+        ("digital", "Livro digital"),
         ("fisico", "Livro fisico")
     )
 
-    tipo = models.CharField(max_length=15, status=status, blank=False, null=False)
+    tipo = models.CharField(max_length=15, choices=status, blank=False, null=False)
 
     def __str__(self):
         return self.tipo
@@ -27,12 +27,12 @@ class Categoria(models.Model):
         ("800", "Literatura: Poesia, romances, contos, crônicas e crítica literária."),
         ("900", " História e Geografia: Biografias, viagens e acontecimentos históricos")
     ]
-    categorias = models.CharField(max_length=200, status=status, null=False, blank=False)
+    categorias = models.CharField(max_length=200, choices=status, null=False, blank=False)
 class Livro(models.Model):
     titulo = models.CharField(max_length=100, null=False, blank=False)
     autor = models.CharField(max_length=200, blank=False, null=False)
     ano = models.IntegerField()
     disponivel = models.BooleanField(default=True)
 
-    categoria = models.ForeignKey(Categoria, blank=False, null=False)
-    tipo = models.ForeignKey(Tipo, blank=False, null=False, default="digital")
+    categoria = models.ForeignKey(Categoria, blank=False, null=False, on_delete=models.CASCADE)
+    tipo = models.ForeignKey(Tipo, blank=False, null=False, default="digital", on_delete=models.CASCADE)
